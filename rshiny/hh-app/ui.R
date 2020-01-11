785d3985e45499280debbec2ae543626faa43a48
Superchordate <superchordate@gmail.com>
2020-01-10 21:39:32 -0600

    Add CSS

--- rshiny/hh-app/ui.R
+++ rshiny/hh-app/ui.R
@@ -5,14 +5,18 @@ rm(i)
 # Define starting UI. Use a function to allow for bookmarking later on if necessary.
 ui = function() shinyUI( fluidPage(
   
-  uihead(),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "general.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "specific.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "shiny-override.css"),
+    tags$title( 'HarrisHack')
+  ),
 
   mainPanel( 
     
-    width = 12, style = 'padding-top:15px', 
-
-    plotlyOutput( 'congress_parliament_plot' )
-    
+    div( style = 'margin: 15px; display: inline-block; ', 
+      plotlyOutput( 'congress_parliament_plot', width = 800, height = 400  )
+    )
   )
   
 ))
